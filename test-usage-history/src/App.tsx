a59ab087be2b7c105b2c4007a8fcea823146d66f
장민우 <jmw900@naver.com>
2022-05-20 16:17:22 +0900

    2022/05/20 TIL : 페이지 기본 데이터 구조 변경

--- test-usage-history/src/App.tsx
+++ test-usage-history/src/App.tsx
@@ -2,11 +2,11 @@ import { ChangeEvent, MouseEvent, useEffect, useState } from 'react';
 import { AppService } from './AppService';
 
 interface defaultInfo {
-  tableTitles : Array<tableTitles>,
-  tableDatas : tableDatas
+  tableTitles: Array<tableTitle>,
+  tableDatas: tableDatas
 };
 
-interface tableTitles {
+interface tableTitle {
   menuType: string,
   name: string,
 };
@@ -18,9 +18,11 @@ interface tableDatas {
 interface tableData {
   menu: Array<string>,
   val: Array<string>,
+  isSelected: Array<boolean>,
 };
 
 interface searchInfo {
+  // [key: string]: string | Array<string>,
   stDate: string,
   endDate: string,
   stateVal: Array<string>,
@@ -32,40 +34,56 @@ function App() {
 
   // 제휴사 여부 확인 - useQuery로 대체 예정
   const [isCompany, setIsCompany] = useState(true);
-  
+
   // 검색메뉴 - 지급방식
-  const [pay, setPay] = useState({ 
-    menu: ['전체', '선불', '착불', '카드', '송금'],
-    val: ['전체', '선불', '착불', '카드', '송금'],
-  });
+  // const [pay, setPay] = useState({ 
+  //   menu: ['전체', '선불', '착불', '카드', '송금'],
+  //   val: ['전체', '선불', '착불', '카드', '송금'],
+  //   isSelected: [false, false, false, false, false],
+  // });
 
   // 제휴사 여부 확인
   useEffect(() => {
 
-    if(isCompany) {
+    if (isCompany) {
       setIsCompany(prev => true);
-      setPay(prev => ({
-        menu: ['전체', '계약', '선불', '착불', '카드', '송금'],
-        val: ['전체', '계약', '선불', '착불', '카드', '송금'],
+      setDefaultInfo(prev => ({
+        ...prev,
+        tableDatas: {
+          ...prev.tableDatas,
+          pay: {
+            ...prev.tableDatas.pay,
+            menu: ['전체', '계약', '선불', '착불', '카드', '송금'],
+            val: ['전체', '계약', '선불', '착불', '카드', '송금'],
+            isSelected: [false, false, false, false, false, false],
+          },
+        }
+
       }));
     };
-  },[isCompany]);
+  }, [isCompany]);
 
   // 검색메뉴
-  const defaultInfo:defaultInfo = {
+  const [defaultInfo, setDefaultInfo] = useState<defaultInfo>({
     tableTitles: [{ menuType: 'date', name: '조회기간' }, { menuType: 'state', name: '조회구분' }, { menuType: 'pay', name: '지급방식' }],
     tableDatas: {
-      date: { 
+      date: {
         menu: ['오늘', '어제', '최근7일', '당월', '전월'],
         val: ['0', '1', '2', '3', '4'],
+        isSelected: [true, false, false, false, false],
       },
-      state: { 
-        menu: ['전체', '완료', '취소', '진행중', '예약'], 
+      state: {
+        menu: ['전체', '완료', '취소', '진행중', '예약'],
         val: ['전체', '완료', '취소', '진행중', '예약'],
+        isSelected: [false, false, false, false, false],
       },
-      pay: pay
+      pay: {
+        menu: ['전체', '선불', '착불', '카드', '송금'],
+        val: ['전체', '선불', '착불', '카드', '송금'],
+        isSelected: [false, false, false, false, false],
+      },
     },
-  };
+  });
 
   // 검색조건
   const [searchInfo, setSearchInfo] = useState<searchInfo>({
@@ -75,41 +93,58 @@ function App() {
     payVal: [],
     searchText: '',
   });
-  
+
   // 검색조건 선택
-  const handleSearchCondition = (e:MouseEvent) => {
+  const handleSearchCondition = (e: MouseEvent) => {
 
     const idx = e.currentTarget.getAttribute('data-idx') as string;
     const menuType = e.currentTarget.getAttribute('data-menutype') as string;
+    const isSelected = defaultInfo.tableDatas[menuType].isSelected;
+    console.log('isSelected', isSelected);
     
+    console.log('isSelected', isSelected);
+
     switch (menuType) {
       case 'date':
-        setSearchInfo(prev => ({ ...prev, stDate: AppService.getDate(idx) }));
+        setSearchInfo(prev => ({ ...prev, stDate: AppService.getDate(idx), endDate: AppService.getDate('0') }));
         break;
       case 'state':
-        setSearchInfo(prev => ({...prev, stateVal: [...prev.stateVal, defaultInfo.tableDatas[menuType].val[idx as unknown as number]]}));
+        setSearchInfo(prev => ({ ...prev, stateVal: [...prev.stateVal, defaultInfo.tableDatas[menuType].val[idx as unknown as number]] }));
         break;
       case 'pay':
-        setSearchInfo(prev => ({...prev, payVal: [...prev.payVal, defaultInfo.tableDatas[menuType].val[idx as unknown as number]]}));
+        setSearchInfo(prev => ({ ...prev, payVal: [...prev.payVal, defaultInfo.tableDatas[menuType].val[idx as unknown as number]] }));
         break;
       default:
         break;
     };
+
+    isSelected[idx as unknown as number] = !isSelected[idx as unknown as number];
+    setDefaultInfo(prev => ({
+      ...prev,
+      tableDatas: {
+        ...prev.tableDatas,
+        [menuType]: {
+          ...prev.tableDatas[menuType],
+          isSelected: isSelected,
+        }
+      },
+    }));
   };
 
-  console.log(searchInfo);
+  console.log(defaultInfo);
+  // console.log(searchInfo);
 
   // 달력날짜 변경
-  const handleCalendar = (e:ChangeEvent<HTMLInputElement>) => {
+  const handleCalendar = (e: ChangeEvent<HTMLInputElement>) => {
 
     const type = e.currentTarget.getAttribute('data-name');
     const selectedValue = (e.target as HTMLInputElement).value;
 
     // type 구분 && 날짜 유효성 검사
-    if (type === 'stDate' && AppService.validateDate(selectedValue, searchInfo.endDate)) {
+    if (type === 'stDate' && AppService.validateDate(selectedValue, searchInfo.endDate as string)) {
       setSearchInfo(prev => ({ ...prev, stDate: selectedValue }));
     }
-    else if (type === 'endDate' && AppService.validateDate(searchInfo.stDate, selectedValue)) {
+    else if (type === 'endDate' && AppService.validateDate(searchInfo.stDate as string, selectedValue)) {
       setSearchInfo(prev => ({ ...prev, endDate: selectedValue }));
     }
     else {
@@ -118,17 +153,17 @@ function App() {
   };
 
   // 제휴사 선택버튼 클릭
-  const handleCompany = (e:MouseEvent<HTMLButtonElement>) => {
+  const handleCompany = (e: MouseEvent<HTMLButtonElement>) => {
     console.log('제휴사 선택');
     // 조회구분 css / 지급방식 css 컨트롤 필요 - searchInfo
-    setSearchInfo(prev => ({...prev, stateVal: [...prev.stateVal, defaultInfo.tableDatas.state.val[0]]}));
-    setSearchInfo(prev => ({...prev, payVal: [...prev.payVal, defaultInfo.tableDatas.pay.val[0], defaultInfo.tableDatas.pay.val[1]]}));
+    setSearchInfo(prev => ({ ...prev, stateVal: [...prev.stateVal, defaultInfo.tableDatas.state.val[0]] }));
+    setSearchInfo(prev => ({ ...prev, payVal: [...prev.payVal, defaultInfo.tableDatas.pay.val[0], defaultInfo.tableDatas.pay.val[1]] }));
   };
 
   return (
     <div className="App">
       {
-        isCompany? <div><p>제휴사 {'>'} 올바로</p><button onClick={handleCompany}>제휴사 선택{'→'}</button></div> : null
+        isCompany ? <div><p>제휴사 {'>'} 올바로</p><button onClick={handleCompany}>제휴사 선택{'→'}</button></div> : null
       }
       <table >
         <tbody >
@@ -138,7 +173,7 @@ function App() {
                 <td>{tableTitle.name}</td>
                 {
                   // 검색메뉴 목록
-                  defaultInfo.tableDatas[tableTitle.menuType].menu.map((data: string, idx:number) => <td onClick={handleSearchCondition} data-idx={idx} data-menutype={tableTitle.menuType} key={data}>{data}</td>)
+                  defaultInfo.tableDatas[tableTitle.menuType].menu.map((item: string, idx: number) => <td className={defaultInfo.tableDatas[tableTitle.menuType].isSelected[idx] ? "active" : ""} onClick={handleSearchCondition} data-idx={idx} data-menutype={tableTitle.menuType} key={item}>{item}</td>)
                 }
                 {
                   // 조회기간 달력추가
